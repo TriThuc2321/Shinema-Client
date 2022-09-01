872bf0144af2f37ef5c3bb59120403bfe3509368
Khanh Linh <72326769+khanhlinh1406@users.noreply.github.com>
2022-09-02 02:38:28 +0700

    add file and fix eslint

--- src/Pages/index.js
+++ src/Pages/index.js
@@ -1,2 +1,6 @@
-export { default as Home } from './Home';
-export { default as Login } from './Login';
+export { default as Home }
+from './Home';
+export { default as Login }
+from './Login';
+export { default as Profile }
+from './Profile';
