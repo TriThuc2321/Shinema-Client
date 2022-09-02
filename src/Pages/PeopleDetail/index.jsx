3c875d4ecec1e2c740f01e754bb6395fe787a6c7
Khanh Linh <72326769+khanhlinh1406@users.noreply.github.com>
2022-09-03 00:42:46 +0700

    init file jsx and style.css

---
+++ src/Pages/PeopleDetail/index.jsx
@@ -0,0 +1,46 @@
+import './style.css';
+import React, { useState, useEffect } from 'react';
+
+import { useParams } from 'react-router';
+import tmdbApi from '../../api/tmdbApi';
+import Person from './Component/person';
+
+function PeopleDetails() {
+    const { id } = useParams();
+    const [person, setPerson] = useState();
+    const [movies, setMovies] = useState([]);
+
+    useEffect(() => {
+        const getPerson = async () => {
+            try {
+                const response = await tmdbApi.getDetailPerson(id);
+                setPerson(response);
+            } catch (err) {
+                console.log(err);
+            }
+        };
+
+        getPerson();
+
+        const getMovie = async () => {
+            try {
+                const response = await tmdbApi.getMovieCredits(id);
+                setMovies(response.cast.filter((item) => (item.backdrop_path !== null || item.poster_path !== null)));
+            } catch (err) {
+                console.log(err);
+            }
+        };
+        getMovie();
+    }, [id]);
+
+
+    return (
+        <div className="details">
+            {
+                person && <Person person = { person } movies = {movies}/>
+            }
+        </div>
+    );
+}
+
+export default PeopleDetails;
