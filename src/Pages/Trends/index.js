159b4cd7bef73f7365d4df007c54eb882e692e7e
Khanh Linh <72326769+khanhlinh1406@users.noreply.github.com>
2022-12-04 22:49:54 +0700

    trends (continue)

--- src/Pages/Trends/index.js
+++ src/Pages/Trends/index.js
@@ -11,9 +11,6 @@ import CountryFilter from './CountryFilter/countryFilter';
 
 function Trends() {
     const [trendsMovie, setTrendsMovie] = useState([]);
-    const [numKW, setNumKW] = useState(0);
-    //const [dictionary, setDictionary] = useState([]);
-    let dictionary = [];
     const [selectedCountry, setSelectedCountry] = useState({
         code: 'US',
         label: 'United States',
@@ -23,92 +20,18 @@ function Trends() {
 
     const getTrends = async (selectedCountry) => {
         // eslint-disable-next-line no-unused-vars
-        let keywordsRes = [];
-        let listKeyword = [];
         await googleTrendsApi
             .getDailyTrends(selectedCountry.code)
-            .then(async (res) => {
-                const listPromise = [];
-                res.data.forEach((trendsWord) => {
-                    listPromise.push(tmdbApi.searchListKeyword(trendsWord));
-                });
-
-                const resKeyword = await Promise.all(listPromise);
-
-                for (let i = 0; i < resKeyword.length; i++) {
-                    if (resKeyword[i].total_results !== 0) {
-                        keywordsRes.push({
-                            trend: res.data[i],
-                            result: resKeyword[i],
-                        });
-                    }
-                }
+            .then((res) => {
+                setTrendsMovie(res.data);
             })
             .catch((err) => console.log(err));
-
-        keywordsRes.map((trendsWordCollection) => {
-            let tmpList = [];
-            const getListKW = () => {
-                trendsWordCollection.result.results.map((item) => tmpList.push({ item }));
-                return tmpList;
-            };
-            listKeyword.push({
-                trend: trendsWordCollection.trend,
-                list: getListKW(),
-            });
-        });
-
-        if (listKeyword.length !== 0) {
-            setNumKW(listKeyword.length);
-            listKeyword.forEach(async (trendKW) => {
-                let listPromise = [];
-                trendKW.list.forEach((keyword) => {
-                    listPromise.push(tmdbApi.discoverWithKeyword(keyword.item.id, selectedCountry.code));
-                });
-                const discoverRes = await Promise.all(listPromise);
-                // dictionary.push({
-                //     keyword: trendKW.trend,
-                //     movies: discoverRes[0].results,
-                // });
-
-                // setDictionary([
-                //     ...dictionary,
-                //     {
-                //         keyword: trendKW.trend,
-                //         movies: discoverRes[0].results,
-                //     },
-                // ]);
-
-                // dictionary.push({
-                //     keyword: trendKW.trend,
-                //     movies: discoverRes[0].results,
-                // });
-
-                setTrendsMovie((currentState) => [
-                    ...currentState,
-                    {
-                        keyword: trendKW.trend,
-                        movies: discoverRes[0].results,
-                    },
-                ]);
-            });
-        }
     };
 
     useEffect(() => {
         setTrendsMovie([]);
         getTrends(selectedCountry);
     }, []);
-
-    // useEffect(() => {
-    //     console.log('numKW', numKW);
-    //     console.log('len', dictionary.length);
-    //     if (numKW === dictionary.length) {
-    //         console.log('aaaaaaa');
-    //         setTrendsMovie([]);
-    //         getTrends(selectedCountry);
-    //     }
-    // }, [dictionary]);
 
     useEffect(() => {
         setTrendsMovie([]);
@@ -129,15 +52,18 @@ function Trends() {
                 <CountryFilter selectedCountry={selectedCountry} setMethod={setSelectedCountry} />
             </div>
 
-            <div class="trends-container">
-                {trendsMovie.length !== 0 &&
-                    trendsMovie.map(
+            {trendsMovie.length !== 0 ? (
+                <div class="trends-container">
+                    {trendsMovie.map(
                         (item) =>
                             item.movies.length !== 0 && (
                                 <MoviesKeyword keyword={item.keyword} movies={item.movies} key={item.keyword} />
                             ),
                     )}
-            </div>
+                </div>
+            ) : (
+                <div class="no-data">No data.</div>
+            )}
         </div>
     );
 }
