88cc4216107df483dfb854009101f266a9f1059d
Khanh Linh <72326769+khanhlinh1406@users.noreply.github.com>
2022-12-11 12:26:42 +0700

    fix trends UI

--- src/Pages/Trends/index.js
+++ src/Pages/Trends/index.js
@@ -3,11 +3,11 @@ import React, { useState, useEffect } from 'react';
 import { Helmet } from 'react-helmet';
 import './trends.css';
 
-import tmdbApi from '~/Api/tmdbApi';
 import googleTrendsApi from '~/Api/googleTrendsApi';
 
 import MoviesKeyword from './MoviesKeyword/moviesKeyword';
 import CountryFilter from './CountryFilter/countryFilter';
+import Loading from '~/Components/Loading';
 
 function Trends() {
     const [trendsMovie, setTrendsMovie] = useState([]);
@@ -17,6 +17,7 @@ function Trends() {
         phone: '1',
         suggested: true,
     });
+    const [isLoading, setIsLoading] = useState(true);
 
     const getTrends = async (selectedCountry) => {
         // eslint-disable-next-line no-unused-vars
@@ -24,16 +25,22 @@ function Trends() {
             .getDailyTrends(selectedCountry.code)
             .then((res) => {
                 setTrendsMovie(res.data);
+                setIsLoading(false);
             })
-            .catch((err) => console.log(err));
+            .catch((err) => {
+                console.log(err);
+                setIsLoading(false);
+            });
     };
 
     useEffect(() => {
+        setIsLoading(true);
         setTrendsMovie([]);
         getTrends(selectedCountry);
     }, []);
 
     useEffect(() => {
+        setIsLoading(true);
         setTrendsMovie([]);
         getTrends(selectedCountry);
     }, [selectedCountry]);
@@ -44,7 +51,7 @@ function Trends() {
     }, [trendsMovie]);
 
     return (
-        <div>
+        <div class="trends-page-container">
             <Helmet>
                 <title>Trends</title>
             </Helmet>
@@ -52,18 +59,22 @@ function Trends() {
                 <CountryFilter selectedCountry={selectedCountry} setMethod={setSelectedCountry} />
             </div>
 
-            {trendsMovie.length !== 0 ? (
-                <div class="trends-container">
-                    {trendsMovie.map(
-                        (item) =>
-                            item.movies.length !== 0 && (
-                                <MoviesKeyword keyword={item.keyword} movies={item.movies} key={item.keyword} />
-                            ),
-                    )}
-                </div>
-            ) : (
-                <div class="no-data">No data.</div>
-            )}
+            <div class="trends-title">Trends in {selectedCountry.label}: </div>
+
+            {isLoading && <Loading />}
+            {!isLoading &&
+                (trendsMovie.length !== 0 ? (
+                    <div class="trends-container">
+                        {trendsMovie.map(
+                            (item) =>
+                                item.movies.length !== 0 && (
+                                    <MoviesKeyword keyword={item.keyword} movies={item.movies} key={item.keyword} />
+                                ),
+                        )}
+                    </div>
+                ) : (
+                    <div class="no-data">No data.</div>
+                ))}
         </div>
     );
 }
